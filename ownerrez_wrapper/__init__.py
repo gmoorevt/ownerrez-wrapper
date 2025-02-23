3b927cb5f9564db0ad4e21d87661ba6c42860cee
Geody Moore <geody.moore@gmail.com>
2025-02-22 19:57:38 -0500

    Release version 0.2.0

--- ownerrez_wrapper/__init__.py
+++ ownerrez_wrapper/__init__.py
@@ -8,8 +8,9 @@ A Python wrapper for the OwnerRez API.
 from .api import API
 from .cli import main as cli
 
-__version__ = ".1.0"
+__version__ = "0.2.0"
 __author__ = "Geody Moore"
-__email__ = "geody.moore@gmail.com"
+__author_email__ = "geody.moore@gmail.com"
+__description__ = "A Python wrapper for the OwnerRez API"
 
 __all__ = ["API", "cli"]
